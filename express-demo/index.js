e69c186665676adeb6b52379cbfbdbd336141fda
soundarya <soundarya.v@digicoffer.com>
2024-08-22 12:49:21 +0530

    added delete

--- express-demo/index.js
+++ express-demo/index.js
@@ -1,3 +1,10 @@
+// Cmd to run code: node index.js
+// Now open browser > localhost:3000
+
+// Run cmd to easy: 
+// sudo npm i -g nodemon | pwd
+// Nodemon index.js
+
 
 const Joi = require('joi');
 const express = require('express');
@@ -11,22 +18,27 @@ const courses = [
     { id:3, name:'Course3' }
 ]
 
+//Just get message: http://localhost:3000
 app.get('/', (req, res) => {
     res.send('Hello World!!!');
 });
 
+//Just get numbers: http://localhost:3000/api/num
+app.get('/api/num', (req, res) => {
+    res.send([1, 2, 3]);
+})
 
-// app.get('/api/courses', (req, res) => {
-//     res.send([1, 2, 3]);
-// })
-
-//Return all courses
+//Get all courses: http://localhost:3000/api/courses
 app.get('/api/courses', (req, res) => {
     res.send(courses);
 })
 
+//Get all courses based on id:http://localhost:3000/api/courses/1
+app.get('/api/courses/:id', (req, res) => {
+    res.send(req.params.id);
+})
 
-//POST courses based on id
+//Add courses: http://localhost:3000/api/courses
 app.post('/api/courses', (req, res) => {
     // const schema = {
     //     name: Joi.string().min(3).required()
@@ -47,21 +59,40 @@ app.post('/api/courses', (req, res) => {
     res.send(course);
  });
 
+ //Update courses: http://localhost:3000/api/courses/id
+ app.put('/api/courses/:id', (req, res) => {
+    const course =  courses.find(c => c.id === parseInt(req.params.id));
+    if(!course) res.status(404).send('The course with given Id was not found!');
+
+    const { error } = validateCourse(req.body);
+    if(error){
+        res.status(400).send(error.details[0].message);
+        return;
+    }
+    
+    course.name = req.body.name;
+    res.send(course);
+ });
+
+ //Delete courses: http://localhost:3000/api/courses
+app.delete('/api/courses/:id', (req, res) => {
+    const course =  courses.find(c => c.id === parseInt(req.params.id));
+    if(!course) res.status(404).send('The course with given Id was not found!')
+
+    const index = courses.indexOf(course);
+    courses.splice(index, 1);
+
+    res.send(course);
+ });
 
 
-
-
-//Get all courses based on id
+//Get courses based on id with error statusMsg:http://localhost:3000/api/courses/1
 app.get('/api/courses/:id', (req, res) => {
    const course = courses.find(c => c.id === parseInt(req.params.id));
    if(!course) res.status(404).send('The course with the given ID was not found!');
    res.send(course);
 })
 
-//Get courses based on id
-app.get('/api/courses/:id', (req, res) => {
-    res.send(req.params.id);
-})
 
 //Get year & month - http://localhost:5000/api/posts/2024/8
 app.get('/api/posts/:year/:month', (req, res) => {
